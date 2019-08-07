269b3905b75538412b8baf993b9ead17e28cb717
Dylan Pinn <dylan@dylanpinn.com>
2019-08-07 19:12:40 +1000

    Lab 1: Q1 WIP

---
+++ labs/lab-01/question-01.py
@@ -0,0 +1,65 @@
+def gusfield(pattern, string):
+  # 3 cases
+  # 1. k > r
+  # 2. k = r
+  # 3. k < r
+
+  # pattern = "aab"
+  string = "aabaabcaxaabaabcy"
+  # concat_string = pattern + "$" + string
+  z_array = [None] * len(string)
+  r_array = [None] * len(string)
+  l_array = [None] * len(string)
+
+  # Base Case
+
+  # match string[2...] with string[1...] until mismatch is found
+
+  match = False
+  i = 1
+  z_array[1] = 0
+  if string[i] == string[i-1]:
+      match = True
+      z_array[1] += 1
+  else:
+      match = False
+      z_array[1] = -1
+  while match:
+      i += 1
+      if string[i] == string[i-1]:
+          z_array[1] += 1
+      else:
+          match = False
+
+  if z_array[1] > 0:
+      r_array[1] = z_array[1]
+      l_array[1] = 1
+  else:
+      r_array[1] = 0
+      l_array[1] = 0
+
+
+  # k = 2
+  # for k in range(len(string)):
+  #   # check case 1
+  #   if k > r:
+  #     # case 1
+  #     # compare strings str[k...q-1] with str[1...q-k] until mismatch
+  #     break;
+  #   elif k == r:
+  #     # case 2
+  #     break;
+  #   elif k < r:
+  #     # case 3
+  #     break;
+  #   else:
+  #     # blow up
+  #     raise Exception('error')
+
+
+  print('string: ', string)
+  print('Z: ', z_array)
+  print('r: ', r_array)
+  print('l: ', l_array)
+
+gusfield('aab', 'aabaabcaxaabaabcy')
