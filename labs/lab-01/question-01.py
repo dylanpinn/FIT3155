4458a91c705d7e6375dddf5054880d42fb1d7e3a
Dylan Pinn <dylan@dylanpinn.com>
2019-08-13 22:07:26 +1000

    Fix indentation

--- labs/lab-01/question-01.py
+++ labs/lab-01/question-01.py
@@ -1,65 +1,68 @@
+# Gusfield Z-Algorithm
+
 def gusfield(pattern, string):
-  # 3 cases
-  # 1. k > r
-  # 2. k = r
-  # 3. k < r
+    # 3 cases
+    # 1. k > r
+    # 2. k = r
+    # 3. k < r
 
-  # pattern = "aab"
-  string = "aabaabcaxaabaabcy"
-  # concat_string = pattern + "$" + string
-  z_array = [None] * len(string)
-  r_array = [None] * len(string)
-  l_array = [None] * len(string)
+    # pattern = "aab"
+    string = "aabaabcaxaabaabcy"
+    # concat_string = pattern + "$" + string
+    z_array = [None] * len(string)
+    r_array = [None] * len(string)
+    l_array = [None] * len(string)
 
-  # Base Case
+    # Base Case
 
-  # match string[2...] with string[1...] until mismatch is found
+    # match string[2...] with string[1...] until mismatch is found
 
-  match = False
-  i = 1
-  z_array[1] = 0
-  if string[i] == string[i-1]:
-      match = True
-      z_array[1] += 1
-  else:
-      match = False
-      z_array[1] = -1
-  while match:
-      i += 1
-      if string[i] == string[i-1]:
-          z_array[1] += 1
-      else:
-          match = False
+    match = False
+    i = 1
+    z_array[1] = 0
+    z_index = 1
+    if string[i] == string[i-z_index]:
+        match = True
+        z_array[z_index] += 1
+    else:
+        match = False
+        z_array[z_index] = -1
+    while match:
+        i += 1
+        if string[i] == string[i-z_index]:
+            z_array[z_index] += 1
+        else:
+            match = False
 
-  if z_array[1] > 0:
-      r_array[1] = z_array[1]
-      l_array[1] = 1
-  else:
-      r_array[1] = 0
-      l_array[1] = 0
-
-
-  # k = 2
-  # for k in range(len(string)):
-  #   # check case 1
-  #   if k > r:
-  #     # case 1
-  #     # compare strings str[k...q-1] with str[1...q-k] until mismatch
-  #     break;
-  #   elif k == r:
-  #     # case 2
-  #     break;
-  #   elif k < r:
-  #     # case 3
-  #     break;
-  #   else:
-  #     # blow up
-  #     raise Exception('error')
+        if z_array[z_index] > 0:
+            r_array[z_index] = z_array[1]
+            l_array[z_index] = 1
+        elif z_array[z_index] == 0:
+            r_array[z_index] = 0
+            l_array[z_index] = 0
 
 
-  print('string: ', string)
-  print('Z: ', z_array)
-  print('r: ', r_array)
-  print('l: ', l_array)
+        # k = 2
+        # for k in range(len(string)):
+        #   # check case 1
+        #   if k > r:
+        #     # case 1
+        #     # compare strings str[k...q-1] with str[1...q-k] until mismatch
+        #     break;
+        #   elif k == r:
+        #     # case 2
+        #     break;
+        #   elif k < r:
+        #     # case 3
+        #     break;
+        #   else:
+        #     # blow up
+        #     raise Exception('error')
+
+
+    print('string: ', string)
+    print('Z: ', z_array)
+    print('l: ', l_array)
+    print('r: ', r_array)
 
 gusfield('aab', 'aabaabcaxaabaabcy')
