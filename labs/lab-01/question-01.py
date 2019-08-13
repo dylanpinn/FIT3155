b759e5c6dc5e8c732f025deb618c1b32fd70d42d
Dylan Pinn <dylan@dylanpinn.com>
2019-08-13 23:06:28 +1000

    L1: Q1 - WIP

--- labs/lab-01/question-01.py
+++ labs/lab-01/question-01.py
@@ -15,7 +15,7 @@ def gusfield(pattern, string):
 
     # Base Case
 
-    # match string[2...] with string[1...] until mismatch is found
+    # match string[1...] with string[0...] until mismatch is found
 
     match = False
     i = 1
@@ -41,24 +41,71 @@ def gusfield(pattern, string):
             r_array[z_index] = 0
             l_array[z_index] = 0
 
+    for k in range(len(string)):
+        if k < 2:
+            continue
 
-        # k = 2
-        # for k in range(len(string)):
-        #   # check case 1
-        #   if k > r:
-        #     # case 1
-        #     # compare strings str[k...q-1] with str[1...q-k] until mismatch
-        #     break;
-        #   elif k == r:
-        #     # case 2
-        #     break;
-        #   elif k < r:
-        #     # case 3
-        #     break;
-        #   else:
-        #     # blow up
-        #     raise Exception('error')
+        # Case 1, if k > r:
+        # Compute Zk by comparing str[k...q-1] with str[1...q-k] until mismatch is found
+        # at some q >= k.
+        # if Zk > 0
+        #   set rk to q-1
+        #   set lk to k
+        # else l,r to previous values
+        if k > r_array[k-1]:
+            match = False
+            z_array[k] = 0
+            i = 0
+            if string[k] == string[1]:
+                match = True
+                z_array[k] += 1
+            else:
+                match = False
+            while match:
+                i += 1
+                if string[k + i] == string[i]:
+                    z_array[k] += 1
+                else:
+                    match = False
+            if z_array[k] > 0:
+                l_array[k] = k
+                r_array[k] = k + i - 1
+            else:
+                l_array[k] = l_array[k-1]
+                r_array[k] = r_array[k-1]
 
+        else:
+            # Case 2a, if Z_{k-l+1} < r-k+1:
+            # set Z_k = Z_{k-l+1}
+            # l, r remain unchanged
+            l = l_array[k-1]
+            r = r_array[k-1]
+            if z_array[k - l + 1] < r - k + 1:
+                z_array[k] = z_array[k - l + 1]
+                l_array[k] = l_array[k-1]
+                r_array[k] = r_array[k-1]
+            else:
+                # Case 2b
+                match = False
+                z_array[k] = 0
+                i = 0
+                # compare str[r+1] with str[r-k+2]
+                if string[r] == string[r - k + 1]:
+                    match = True
+                else:
+                    match = False
+                while match:
+                    i += 1
+                    if string[r + i] == string[r - k + 1 + i]:
+                        match = True
+                    else:
+                        match = False
+
+                q = r + i
+
+                z_array[k] = q - k
+                l_array[k] = k
+                r_array[k] = q
 
     print('string: ', string)
     print('Z: ', z_array)
