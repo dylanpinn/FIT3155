04131352287a39368443264f6268bdb510ce2440
Dylan Pinn <me@dylanpinn.com>
2019-09-08 23:10:23 +1000

    Remove bandit checking

--- assignments/assignment-01/q1/boyermoore_test.py
+++ assignments/assignment-01/q1/boyermoore_test.py
@@ -9,7 +9,7 @@ import boyermoore
 def random_string(string_length=10):
     """Generates a random string of a fixed length."""
     chars = string.ascii_letters + string.digits
-    return ''.join(random.choice(chars) for i in range(string_length))  # nosec
+    return ''.join(random.choice(chars) for i in range(string_length))
 
 
 class TestBoyerMoore:
@@ -18,61 +18,61 @@ class TestBoyerMoore:
     def test_match(self):
         pat = 'abc'
         txt = 'abcdabcdabcd'
-        assert boyermoore.match(pat, txt) == bool(pat in txt)  # nosec
+        assert boyermoore.match(pat, txt) == bool(pat in txt)
 
     def test_no_match(self):
         pat = 'abc'
         txt = 'abdaabdc'
-        assert boyermoore.match(pat, txt) == bool(pat in txt)  # nosec
+        assert boyermoore.match(pat, txt) == bool(pat in txt)
 
     def test_boyer_match(self):
         """Test matches on first iteration."""
         pat = 'abc'
         txt = 'abcdabc'
-        assert boyermoore.match(pat, txt) is True  # nosec
+        assert boyermoore.match(pat, txt) is True
 
     def test_match_2(self):
         """Test matches on further iteration."""
         pat = 'abc'
         txt = 'abcdabcd'
-        assert boyermoore.match(pat, txt) is True  # nosec
+        assert boyermoore.match(pat, txt) is True
 
     def test_no_match_6(self):
         """Test no matches."""
         pat = 'abc'
         txt = 'abdabdabe'
-        assert boyermoore.match(pat, txt) is False  # nosec
+        assert boyermoore.match(pat, txt) is False
 
     def test_incorrect_len(self):
         """Test no match when pattern > text."""
         pat = 'abcd'
         txt = 'abc'
-        assert boyermoore.match(pat, txt) is False  # nosec
+        assert boyermoore.match(pat, txt) is False
 
     def test_no_match_2(self):
         """Test no match when pattern > text."""
         pat = 'Ap'
         txt = 'poFfTzfHQAOD9Duwe9eB3gRaJIGIgyW35DWJjplV'
-        assert boyermoore.match(pat, txt) is False  # nosec
+        assert boyermoore.match(pat, txt) is False
 
     def test_no_match_3(self):
         """Test no match when pattern > text."""
         pat = '6FjKojoBhRk8YbMH9fau0fHk9S38S5LcJ2LSzOApSw9ScEOlN4p0bKbbLlmurKYG0epr5O3RrU2avmQA1pPK02'
         txt = '02mTAg8avVRF01uKrsuuLJzKU36WzL6VUJiHvBDQxfA7PoN4vy9JR7oEg3x76yeRsEOOoDRNmwfXgEXIlmtjJrEwSp7ptRwFquP8u0'
-        assert boyermoore.match(pat, txt) == bool(pat in txt)  # nosec
+        assert boyermoore.match(pat, txt) == bool(pat in txt)
 
     def test_no_match_4(self):
         """Test no match when pattern > text."""
         pat = 'l'
         txt = 'lFx0OyuczjmH'
-        assert boyermoore.match(pat, txt) == bool(pat in txt)  # nosec
+        assert boyermoore.match(pat, txt) == bool(pat in txt)
 
     def test_match_all(self):
         for i in range(2, 1000):
-            pat = random_string(random.randint(1, i))  # nosec
+            pat = random_string(random.randint(1, i))
             txt = random_string(i)
             expected = bool(pat in txt)
-            assert boyermoore.match(pat, txt) == expected  # nosec
+            assert boyermoore.match(pat, txt) == expected
 
     def test_bad_char(self):
         expected = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
@@ -94,7 +94,7 @@ class TestBoyerMoore:
                     -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
                     -1]
         pat = 'ABC'
-        assert boyermoore.calculate_bad_char_shift(pat) == expected  # nosec
+        assert boyermoore.calculate_bad_char_shift(pat) == expected
 
     # TODO: Test good suffix
     # TODO: Test matched prefix
@@ -103,66 +103,66 @@ class TestBoyerMoore:
         pat = 'abc'
         txt = 'abcdabcdabcd'
         expected = [1, 5, 9]
-        assert boyermoore.boyer_moore(pat, txt) == expected  # nosec
+        assert boyermoore.boyer_moore(pat, txt) == expected
 
     def test_boyer_match_1(self):
         pat = 'abc'
         txt = 'abcdabcdabcd'
         expected = [m.start() for m in re.finditer(pat, txt)]
-        assert boyermoore.boyer_moore(pat, txt, 0) == expected  # nosec
+        assert boyermoore.boyer_moore(pat, txt, 0) == expected
 
     def test_boyer_match_0(self):
         """Test matches on first iteration."""
         pat = 'abc'
         txt = 'abcdabc'
         expected = [m.start() for m in re.finditer(pat, txt)]
-        assert boyermoore.boyer_moore(pat, txt, 0) == expected  # nosec
+        assert boyermoore.boyer_moore(pat, txt, 0) == expected
 
     def test_boyer_match_2(self):
         """Test matches on further iteration."""
         pat = 'abc'
         txt = 'abcdabcd'
         expected = [m.start() for m in re.finditer(pat, txt)]
-        assert boyermoore.boyer_moore(pat, txt, 0) == expected  # nosec
+        assert boyermoore.boyer_moore(pat, txt, 0) == expected
 
     def test_boyer_no_match(self):
         """Test no matches."""
         pat = 'abc'
         txt = 'abdabdabe'
         expected = [m.start() for m in re.finditer(pat, txt)]
-        assert boyermoore.boyer_moore(pat, txt, 0) == expected  # nosec
+        assert boyermoore.boyer_moore(pat, txt, 0) == expected
 
     def test_boyer_incorrect_len(self):
         """Test no match when pattern > text."""
         pat = 'abcd'
         txt = 'abc'
         expected = [m.start() for m in re.finditer(pat, txt)]
-        assert boyermoore.boyer_moore(pat, txt, 0) == expected  # nosec
+        assert boyermoore.boyer_moore(pat, txt, 0) == expected
 
     def test_boyer_no_match_2(self):
         """Test no match when pattern > text."""
         pat = 'Ap'
         txt = 'poFfTzfHQAOD9Duwe9eB3gRaJIGIgyW35DWJjplV'
         expected = [m.start() for m in re.finditer(pat, txt)]
-        assert boyermoore.boyer_moore(pat, txt, 0) == expected  # nosec
+        assert boyermoore.boyer_moore(pat, txt, 0) == expected
 
     def test_boyer_no_match_3(self):
         """Test no match when pattern > text."""
         pat = '6FjKojoBhRk8YbMH9fau0fHk9S38S5LcJ2LSzOApSw9ScEOlN4p0bKbbLlmurKYG0epr5O3RrU2avmQA1pPK02'
         txt = '02mTAg8avVRF01uKrsuuLJzKU36WzL6VUJiHvBDQxfA7PoN4vy9JR7oEg3x76yeRsEOOoDRNmwfXgEXIlmtjJrEwSp7ptRwFquP8u0'
         expected = [m.start() for m in re.finditer(pat, txt)]
-        assert boyermoore.boyer_moore(pat, txt, 0) == expected  # nosec
+        assert boyermoore.boyer_moore(pat, txt, 0) == expected
 
     def test_boyer_no_match_4(self):
         """Test no match when pattern > text."""
         pat = 'l'
         txt = 'lFx0OyuczjmH'
         expected = [m.start() for m in re.finditer(pat, txt)]
-        assert boyermoore.boyer_moore(pat, txt, 0) == expected  # nosec
+        assert boyermoore.boyer_moore(pat, txt, 0) == expected
 
     def test_all_boyer(self):
         for i in range(2, 1000):
-            pat = random_string(random.randint(1, i))  # nosec
+            pat = random_string(random.randint(1, i))
             txt = random_string(i)
             expected = [m.start() for m in re.finditer(pat, txt)]
-            assert boyermoore.boyer_moore(pat, txt, 0) == expected  # nosec
+            assert boyermoore.boyer_moore(pat, txt, 0) == expected
--- assignments/assignment-01/q2/modified_kmp_test.py
+++ assignments/assignment-01/q2/modified_kmp_test.py
@@ -9,7 +9,7 @@ import modified_kmp
 def random_string(string_length=10):
     """Generates a random string of a fixed length."""
     chars = string.ascii_letters + string.digits
-    return ''.join(random.choice(chars) for i in range(string_length))  # nosec
+    return ''.join(random.choice(chars) for i in range(string_length))
 
 
 class TestModifiedKMP:
@@ -21,54 +21,54 @@ class TestModifiedKMP:
     def test_match(self):
         pat = 'abc'
         txt = 'abcdabcdabcd'
-        assert modified_kmp.match(pat, txt) == bool(pat in txt)  # nosec
+        assert modified_kmp.match(pat, txt) == bool(pat in txt)
 
     def test_no_match(self):
         pat = 'abc'
         txt = 'abdaabdc'
-        assert modified_kmp.match(pat, txt) == bool(pat in txt)  # nosec
+        assert modified_kmp.match(pat, txt) == bool(pat in txt)
 
     def test_kmp_match(self):
         """Test matches on first iteration."""
         pat = 'abc'
         txt = 'abcdabc'
-        assert modified_kmp.match(pat, txt) is True  # nosec
+        assert modified_kmp.match(pat, txt) is True
 
     def test_match_2(self):
         """Test matches on further iteration."""
         pat = 'abc'
         txt = 'abcdabcd'
-        assert modified_kmp.match(pat, txt) is True  # nosec
+        assert modified_kmp.match(pat, txt) is True
 
     def test_no_match_6(self):
         """Test no matches."""
         pat = 'abc'
         txt = 'abdabdabe'
-        assert modified_kmp.match(pat, txt) is False  # nosec
+        assert modified_kmp.match(pat, txt) is False
 
     def test_incorrect_len(self):
         """Test no match when pattern > text."""
         pat = 'abcd'
         txt = 'abc'
-        assert modified_kmp.match(pat, txt) is False  # nosec
+        assert modified_kmp.match(pat, txt) is False
 
     def test_no_match_2(self):
         """Test no match when pattern > text."""
         pat = 'Ap'
         txt = 'poFfTzfHQAOD9Duwe9eB3gRaJIGIgyW35DWJjplV'
-        assert modified_kmp.match(pat, txt) is False  # nosec
+        assert modified_kmp.match(pat, txt) is False
 
     def test_no_match_3(self):
         """Test no match when pattern > text."""
         pat = '6FjKojoBhRk8YbMH9fau0fHk9S38S5LcJ2LSzOApSw9ScEOlN4p0bKbbLlmurKYG0epr5O3RrU2avmQA1pPK02'
         txt = '02mTAg8avVRF01uKrsuuLJzKU36WzL6VUJiHvBDQxfA7PoN4vy9JR7oEg3x76yeRsEOOoDRNmwfXgEXIlmtjJrEwSp7ptRwFquP8u0'
-        assert modified_kmp.match(pat, txt) == bool(pat in txt)  # nosec
+        assert modified_kmp.match(pat, txt) == bool(pat in txt)
 
     def test_no_match_4(self):
         """Test no match when pattern > text."""
         pat = 'l'
         txt = 'lFx0OyuczjmH'
-        assert modified_kmp.match(pat, txt) == bool(pat in txt)  # nosec
+        assert modified_kmp.match(pat, txt) == bool(pat in txt)
 
     @pytest.mark.skip("TODO: Fix")
     def test_match_kmp(self):
@@ -78,83 +78,83 @@ class TestModifiedKMP:
 
     def test_match_all(self):
         for i in range(2, 1000):
-            pat = random_string(random.randint(1, i))  # nosec
+            pat = random_string(random.randint(1, i))
             txt = random_string(i)
             expected = bool(pat in txt)
-            assert modified_kmp.match(pat, txt) == expected  # nosec
+            assert modified_kmp.match(pat, txt) == expected
 
     def test_kmp_algorithm(self):
         pat = 'abc'
         txt = 'abcdabcdabcd'
         expected = [1, 5, 9]
-        assert modified_kmp.kmp(pat, txt) == expected  # nosec
+        assert modified_kmp.kmp(pat, txt) == expected
 
     @pytest.mark.skip("TODO: Fix")
     def test_kmp_algorithm_other(self):
         pat = 'abcdabcy'
         txt = 'abcxabcdabcdabcy'
         expected = [m.start() for m in re.finditer(pat, txt)]
-        assert modified_kmp.kmp(pat, txt, 0) == expected  # nosec
+        assert modified_kmp.kmp(pat, txt, 0) == expected
 
     def test_kmp_match_1(self):
         pat = 'abc'
         txt = 'abcdabcdabcd'
         expected = [m.start() for m in re.finditer(pat, txt)]
-        assert modified_kmp.kmp(pat, txt, 0) == expected  # nosec
+        assert modified_kmp.kmp(pat, txt, 0) == expected
 
     def test_kmp_match_0(self):
         """Test matches on first iteration."""
         pat = 'abc'
         txt = 'abcdabc'
         expected = [m.start() for m in re.finditer(pat, txt)]
-        assert modified_kmp.kmp(pat, txt, 0) == expected  # nosec
+        assert modified_kmp.kmp(pat, txt, 0) == expected
 
     def test_kmp_match_2(self):
         """Test matches on further iteration."""
         pat = 'abc'
         txt = 'abcdabcd'
         expected = [m.start() for m in re.finditer(pat, txt)]
-        assert modified_kmp.kmp(pat, txt, 0) == expected  # nosec
+        assert modified_kmp.kmp(pat, txt, 0) == expected
 
     def test_kmp_no_match(self):
         """Test no matches."""
         pat = 'abc'
         txt = 'abdabdabe'
         expected = [m.start() for m in re.finditer(pat, txt)]
-        assert modified_kmp.kmp(pat, txt, 0) == expected  # nosec
+        assert modified_kmp.kmp(pat, txt, 0) == expected
 
     def test_kmp_incorrect_len(self):
         """Test no match when pattern > text."""
         pat = 'abcd'
         txt = 'abc'
         expected = [m.start() for m in re.finditer(pat, txt)]
-        assert modified_kmp.kmp(pat, txt, 0) == expected  # nosec
+        assert modified_kmp.kmp(pat, txt, 0) == expected
 
     def test_kmp_no_match_2(self):
         """Test no match when pattern > text."""
         pat = 'Ap'
         txt = 'poFfTzfHQAOD9Duwe9eB3gRaJIGIgyW35DWJjplV'
         expected = [m.start() for m in re.finditer(pat, txt)]
-        assert modified_kmp.kmp(pat, txt, 0) == expected  # nosec
+        assert modified_kmp.kmp(pat, txt, 0) == expected
 
     def test_kmp_no_match_3(self):
         """Test no match when pattern > text."""
         pat = '6FjKojoBhRk8YbMH9fau0fHk9S38S5LcJ2LSzOApSw9ScEOlN4p0bKbbLlmurKYG0epr5O3RrU2avmQA1pPK02'
         txt = '02mTAg8avVRF01uKrsuuLJzKU36WzL6VUJiHvBDQxfA7PoN4vy9JR7oEg3x76yeRsEOOoDRNmwfXgEXIlmtjJrEwSp7ptRwFquP8u0'
         expected = [m.start() for m in re.finditer(pat, txt)]
-        assert modified_kmp.kmp(pat, txt, 0) == expected  # nosec
+        assert modified_kmp.kmp(pat, txt, 0) == expected
 
     def test_kmp_no_match_4(self):
         """Test no match when pattern > text."""
         pat = 'l'
         txt = 'lFx0OyuczjmH'
         expected = [m.start() for m in re.finditer(pat, txt)]
-        assert modified_kmp.kmp(pat, txt, 0) == expected  # nosec
+        assert modified_kmp.kmp(pat, txt, 0) == expected
 
     @pytest.mark.skip("TODO: Fix")
     def test_all_kmp(self):
         for i in range(2, 1000):
-            pat = random_string(random.randint(1, i))  # nosec
+            pat = random_string(random.randint(1, i))
             txt = random_string(i)
             expected = [m.start() for m in re.finditer(pat, txt)]
-            assert modified_kmp.kmp(pat, txt, 0) == expected  # nosec
+            assert modified_kmp.kmp(pat, txt, 0) == expected
