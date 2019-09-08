e7d89b6d3b005707177c4a7495e46fb2e445e26d
Dylan Pinn <me@dylanpinn.com>
2019-09-08 14:51:19 +1000

    KMP Naive working

--- assignments/assignment-01/q2/modified_kmp.py
+++ assignments/assignment-01/q2/modified_kmp.py
@@ -3,9 +3,15 @@
 """
 
 
-def kmp(pat, text):
-    return True
-
-
 def match(pat, txt):
-    return True
+    """Does pattern exist in text."""
+    n = len(txt) - 1
+    m = len(pat) - 1
+
+    for i in range(0, n - m + 1):
+        for j in range(0, m + 1):
+            if txt[i + j] != pat[j]:
+                break
+            if j == m:
+                return True
+    return False
