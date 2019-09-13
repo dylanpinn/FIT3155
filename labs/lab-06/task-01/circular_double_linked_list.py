a570be8ffc680f47a1c23db8d6513e40a826e355
Dylan Pinn <me@dylanpinn.com>
2019-09-13 12:23:10 +1000

    Make list circular

--- labs/lab-06/task-01/circular_double_linked_list.py
+++ labs/lab-06/task-01/circular_double_linked_list.py
@@ -1,45 +1,61 @@
 """Circular Double Linked List."""
 
+from node import Node
+
 
 class CircularDoubleLinkedList:
     """Circular Double Linked List."""
 
     def __init__(self):
         self.size = 0
-        self.first = None
         self.last = None
 
-    def insert_beginning(self, node):
-        """Insert a node at the 'start' of the list."""
-        if self.first is None:
-            self.first = node
-            self.last = node
-            node.prev = None
-            node.next = None
-            self.size += 1
-        else:
-            self.insert_before(self.first, node)
-
     def insert_before(self, node, new_node):
-        """Insert a new node before an existing node."""
-        new_node.next = node
-        if node.prev is None:
-            new_node.prev = None
-            self.first = new_node
-        else:
-            new_node.prev = node.prev
-            node.prev.next = new_node
-        node.prev = new_node
+        """Insert a new node before an existing node.
+        :param node: Existing Node
+        :type node: Node
+        :param new_node: New Node to insert.
+        :type node: Node
+        """
+        self.insert_after(node.prev, new_node)
+
+    def insert_after(self, node, new_node):
+        """Insert a new node after an existing node.
+        :param node: Existing Node
+        :type node: Node
+        :param new_node: New Node to insert.
+        :type new_node: Node
+        """
+        new_node.next = node.next
+        new_node.prev = node
+        node.next.prev = new_node
+        node.next = new_node
         self.size += 1
 
-    def remove(self, node):
-        """Remove node from list."""
-        if node.prev is None:
-            self.first = node.next
+    def insert_end(self, node):
+        """Insert a node at the end of the list.
+        :param node: New Node to insert.
+        :type node: Node
+        """
+        if self.last is None:
+            node.prev = node
+            node.next = node
+            self.size += 1
         else:
-            node.prev.next = node.next
-        if node.next is None:
-            self.last = node.prev
+            self.insert_after(self.last, node)
+        self.last = node
+
+    def remove(self, node):
+        """Remove node from list.
+        :param node: Node to remove from list.
+        :type node: Node
+        """
+        if node.next == node:
+            self.last = None
         else:
             node.next.prev = node.prev
+            node.prev.next = node.next
+            if node == self.last:
+                self.last = node.prev
+        del node
         self.size -= 1
--- labs/lab-06/task-01/circular_double_linked_list_test.py
+++ labs/lab-06/task-01/circular_double_linked_list_test.py
@@ -13,11 +13,6 @@ class TestCircularDoubleLinkedList:
         linked_list = CircularDoubleLinkedList()
         assert linked_list.size == 0
 
-    def test_initial_first(self):
-        """Lists have no initial first node."""
-        linked_list = CircularDoubleLinkedList()
-        assert linked_list.first is None
-
     def test_initial_last(self):
         """Lists have no initial last node."""
         linked_list = CircularDoubleLinkedList()
@@ -27,64 +22,67 @@ class TestCircularDoubleLinkedList:
         """Test inserting into an empty list."""
         linked_list = CircularDoubleLinkedList()
         node = Node(10)
-        linked_list.insert_beginning(node)
-        assert linked_list.first is node
+        linked_list.insert_end(node)
         assert linked_list.last is node
         assert linked_list.size == 1
-        assert node.prev is None
-        assert node.next is None
+        assert node.prev is node
+        assert node.next is node
 
-    def test_insert_beginning(self):
-        """Test inserting into a list at beginning."""
+    def test_insert_end(self):
+        """Test inserting into a list at end."""
         linked_list = CircularDoubleLinkedList()
         node1 = Node(10)
-        linked_list.insert_beginning(node1)
+        linked_list.insert_end(node1)
         node2 = Node(5)
-        linked_list.insert_beginning(node2)
-        assert linked_list.first is node2
-        assert linked_list.last is node1
+        linked_list.insert_end(node2)
+        assert linked_list.last is node2
+        assert linked_list.last.next is node1
         assert linked_list.size == 2
 
     def test_insert_before(self):
         """Test inserting before an existing list."""
         linked_list = CircularDoubleLinkedList()
         node1 = Node(10)
-        linked_list.insert_beginning(node1)
+        linked_list.insert_end(node1)
         node2 = Node(5)
-        linked_list.insert_beginning(node2)
+        linked_list.insert_end(node2)
         node3 = Node(9)
         linked_list.insert_before(node1, node3)
-        assert linked_list.first is node2
-        assert linked_list.last is node1
+        assert linked_list.last is node2
+        assert linked_list.last.next is node3
+        assert linked_list.last.next.next is node1
+        assert linked_list.last.next.next.next is node2
         assert linked_list.size == 3
 
     def test_remove_item_first(self):
         """Test removing first item in list."""
         linked_list = CircularDoubleLinkedList()
         node1 = Node(10)
-        linked_list.insert_beginning(node1)
+        linked_list.insert_end(node1)
         node2 = Node(5)
-        linked_list.insert_beginning(node2)
+        linked_list.insert_end(node2)
         node3 = Node(9)
-        linked_list.insert_beginning(node3)
-        assert linked_list.first is node3
+        linked_list.insert_end(node3)
+        assert linked_list.last is node3
+        assert linked_list.last.next is node1
         assert linked_list.size == 3
-        linked_list.remove(node3)
-        assert linked_list.first is node2
+        linked_list.remove(node1)
+        assert linked_list.last is node3
+        assert linked_list.last.next is node2
         assert linked_list.size == 2
 
     def test_remove_item_last(self):
         """Test removing last item in list."""
         linked_list = CircularDoubleLinkedList()
-        node1 = Node(10)
-        linked_list.insert_beginning(node1)
-        node2 = Node(5)
-        linked_list.insert_beginning(node2)
-        node3 = Node(9)
-        linked_list.insert_beginning(node3)
-        assert linked_list.last is node1
+        node1 = Node(1)
+        linked_list.insert_end(node1)
+        node2 = Node(2)
+        linked_list.insert_end(node2)
+        node3 = Node(3)
+        linked_list.insert_end(node3)
+        assert linked_list.last is node3
         assert linked_list.size == 3
-        linked_list.remove(node1)
+        linked_list.remove(node3)
         assert linked_list.last is node2
         assert linked_list.size == 2
 
@@ -92,15 +90,16 @@ class TestCircularDoubleLinkedList:
         """Test removing a item in middle of the list."""
         linked_list = CircularDoubleLinkedList()
         node1 = Node(10)
-        linked_list.insert_beginning(node1)
-        node2 = Node(5)
-        linked_list.insert_beginning(node2)
-        node3 = Node(9)
-        linked_list.insert_beginning(node3)
-        assert linked_list.first is node3
-        assert linked_list.last is node1
+        linked_list.insert_end(node1)
+        node2 = Node(9)
+        linked_list.insert_end(node2)
+        node3 = Node(8)
+        linked_list.insert_end(node3)
+        assert linked_list.last.next is node1
+        assert linked_list.last is node3
         assert linked_list.size == 3
         linked_list.remove(node2)
-        assert linked_list.first is node3
-        assert linked_list.last is node1
+        assert linked_list.last is node3
+        assert linked_list.last.next is node1
+        assert linked_list.last.next.next is node3
         assert linked_list.size == 2
