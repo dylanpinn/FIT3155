b3102d4508dee59ee8f9b1d7dab72b0a699d2785
Dylan Pinn <me@dylanpinn.com>
2019-10-03 12:54:57 +1000

    Generate strings of correct length

---
+++ labs/lab-01/question_03.py
@@ -0,0 +1,19 @@
+"""
+FIT3155 - Lab 2
+
+Question 3
+
+Write a program to generate a random string of any stated length n
+from a binary alphabet {H, T}, with the probability of character H
+being p (and hence probability of T is 1 − p). Your program should
+take n and p as arguments to write out a random string to a file
+
+Dylan Pinn - 24160547
+"""
+
+from random import choice
+
+
+def generate(n, p):
+    string = ''.join(choice(['H', 'T']) for i in range(n))
+    return string
