9894765e4d65cd34938a8d3bde2d7567bc4a59cb
Dylan Pinn <me@dylanpinn.com>
2019-10-13 19:01:36 +1100

    WIP Task 3

---
+++ labs/lab-10/huffman_coding_test.py
@@ -0,0 +1,18 @@
+import huffman_coding
+
+
+class TestHuffmanCoding:
+    def test_number_of_unique_chars(self):
+        code = "A_DEAD_DAD_CEDED_A_BAD_BABE_A_BEADED_ABACA_BED"
+        result = huffman_coding.unique_chars(code)
+        assert len(result) == 6
+
+    def test_freq_of_chars(self):
+        code = "A_DEAD_DAD_CEDED_A_BAD_BABE_A_BEADED_ABACA_BED"
+        result = huffman_coding.unique_chars(code)
+        assert result["C"] == 2
+        assert result["B"] == 6
+        assert result["E"] == 7
+        assert result["_"] == 10
+        assert result["D"] == 10
+        assert result["A"] == 11
