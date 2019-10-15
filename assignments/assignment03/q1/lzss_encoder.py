7b3346f2f1c3027be6c26ce74f794934c6e2f2f9
Dylan Pinn <me@dylanpinn.com>
2019-10-16 10:07:16 +1100

    WIP

--- assignments/assignment03/q1/lzss_encoder.py
+++ assignments/assignment03/q1/lzss_encoder.py
@@ -10,32 +10,74 @@ import elias_encoder
 import huffman_coding
 
 
-def encoder(code: str, window_size: int, buffer_size: int):
-    pass
+class LZSSEncoder:
+    def __init__(self, code: str, window_size: int, buffer_size: int):
+        self.code = code
+        self.window_size = window_size
+        self.buffer_size = buffer_size
+        self.huffman_values = huffman_coding.encoded_values(code)[0]
 
+    def encode_header(self) -> str:
+        header = ""
+        unique_chars = huffman_coding.unique_chars(self.code)
+        # Encode the number of unique values in the input txt
+        header += elias_encoder.encode_single_value(len(unique_chars))
+        for val in sorted(unique_chars):
+            # Encode the 8-bit ASCII code
+            header += self.convert_to_8_bit_ascii(val)
+            huff_code = self.huffman_values[val]
+            # Encode the length of the Huffman code
+            header += elias_encoder.encode_single_value(len(huff_code))
+            # Add the Huffman code
+            header += huff_code
 
-def encode_header(code, huffman_values) -> str:
-    header = ""
-    unique_chars = huffman_coding.unique_chars(code)
-    # Encode the number of unique values in the input txt
-    header += elias_encoder.encode_single_value(len(unique_chars))
-    for val in sorted(unique_chars):
-        # Encode the 8-bit ASCII code
-        header += convert_to_8_bit_ascii(val)
-        huff_code = huffman_values[val]
-        # Encode the length of the Huffman code
-        header += elias_encoder.encode_single_value(len(huff_code))
-        # Add the Huffman code
-        header += huff_code
+        return header
 
-    return header
+    def encode_data(self):
+        data = ""
+        encoding = self.lz77_encoding()
 
+        return data
 
-def convert_to_8_bit_ascii(char: str) -> str:
-    return "{0:0=8d}".format(int(bin(ord(char))[2:]))
+    @staticmethod
+    def convert_to_8_bit_ascii(char: str) -> str:
+        return "{0:0=8d}".format(int(bin(ord(char))[2:]))
 
+    def lz77_encoding(self):
+        i = 0
+        encoding = []
+        while i < len(self.code):
+            #     prefix := longest prefix of input that begins in window
+            prefix = self.find_prefix(i)
 
-def encode_data(code: str, window_size: int, buffer_size: int) -> str:
-    data = ""
+            #     if prefix exists then
+            #         i := distance to start of prefix
+            #         l := length of prefix
+            #         c := char following prefix in input
+            if prefix:
+                val = prefix
+            else:
+                val = (0, 0, self.code[i])
 
-    return data
+            encoding.append(val)
+
+            #     s := pop l+1 chars from front of input
+            #     discard l+1 chars from front of window
+            #     append s to back of window
+            i += val[1] + 1
+        return encoding
+
+    def find_prefix(self, current_position: int):
+        end_of_search = current_position + self.buffer_size
+        longest_match = ()
+        start_position = max(0, current_position - self.window_size)
+
+        for i in range(current_position, start_position, -1):
+            for j in range(current_position, end_of_search):
+                if self.code[i - 1] != self.code[j]:
+                    break
+                longest_match = (i - j + 1, i - j + 1, self.code[i + 1])
+
+        if longest_match:
+            return longest_match
+        return False
--- assignments/assignment03/q1/lzss_encoder_test.py
+++ assignments/assignment03/q1/lzss_encoder_test.py
@@ -5,13 +5,16 @@ import huffman_coding
 class TestLZSSEncoder:
     def test_example_header(self):
         code = "aacaacabcaba"
-        huffman_values = huffman_coding.encoded_values(code)[0]
-        result = lzss_encoder.encode_header(code, huffman_values)
+        window_size = 6
+        look_buffer = 4
+        encoder = lzss_encoder.LZSSEncoder(code, window_size, look_buffer)
+        result = encoder.encode_header()
         assert "011011000011101100010010000110001101001" == result
 
     def test_example_data(self):
         code = "aacaacabcaba"
         window_size = 6
         look_buffer = 4
-        result = lzss_encoder.encode_data(code, window_size, look_buffer)
+        encoder = lzss_encoder.LZSSEncoder(code, window_size, look_buffer)
+        result = encoder.encode_data()
         assert "00011111111010011000100100001101111" == result
