ddd25eaf79d13c77c40a2ae845f3fc40cd55d077
Dylan Pinn <me@dylanpinn.com>
2019-10-26 13:28:06 +1100

    WIP Decode

---
+++ assignments/assignment03/q1/lzss_decoder.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+FIT3155 - Assignment 3
+
+Semester 2 2019
+
+Dylan Pinn 24160547
+
+
+Q1: LZSS Decoder
+"""
+
+
+class Decoder:
+    def __init__(self, code: str):
+        self.code = code
+
+    def decode(self) -> str:
+        return ""
