c0439b270ba70ac418a96b79b1f1c5476a2a9e41
Dylan Pinn <me@dylanpinn.com>
2019-10-26 12:42:59 +1100

    Convert to class

--- assignments/assignment03/q1/lzss_encoder.py
+++ assignments/assignment03/q1/lzss_encoder.py
@@ -14,43 +14,54 @@ Q1: LZSS Encoder
 from . import elias_encoder, huffman_coding
 
 
-def encode_header(string: str, window_size: int, buffer_size: int) -> str:
-    """
-    Encode string into Header.
-    - Encode using variable-length Elias, the no of unique ASCII characters.
-    - For each unique char
-    -- Encode using fixed-length 8-bit ASCII code the unique char
-    -- Encode using variable-length Elias code the length of the Huffman code
-       assigned to that unique char
-    """
-    result = ""
+class Encoder:
+    def __init__(self, string: str, window_size: int, buffer_size: int):
+        self.string = string
+        self.window_size = window_size
+        self.buffer_size = buffer_size
 
-    unique_chars = huffman_coding.unique_chars(string)
-    unique_chars_list = list(unique_chars)
-    unique_chars_list.sort()
-    no_of_unique = len(unique_chars_list)
+    def encode(self):
+        result = self.encode_header()
+        result += self.encode_data()
+        return result
 
-    huffman_values = huffman_coding.encoded_values(string)[0]
-    print(huffman_values)
+    def encode_header(self) -> str:
+        """
+        Encode string into Header.
+        - Encode using variable-length Elias, the no of unique ASCII characters.
+        - For each unique char
+        -- Encode using fixed-length 8-bit ASCII code the unique char
+        -- Encode using variable-length Elias code the length of the Huffman code
+           assigned to that unique char
+        """
+        result = ""
 
-    result += elias_encoder.encode_single_value(no_of_unique)
+        unique_chars = huffman_coding.unique_chars(self.string)
+        unique_chars_list = list(unique_chars)
+        unique_chars_list.sort()
+        no_of_unique = len(unique_chars_list)
 
-    for char in unique_chars_list:
-        ascii_8_bit = convert_to_8_bit_ascii(char)
-        huffman_code = huffman_values[char]
-        encoded_huffman_len = elias_encoder.encode_single_value(
-            len(huffman_code)
-        )
+        huffman_values = huffman_coding.encoded_values(self.string)[0]
+        print(huffman_values)
 
-        result += ascii_8_bit
-        result += encoded_huffman_len
-        result += huffman_code
-        print(ascii_8_bit)
-        print(encoded_huffman_len)
-        print(huffman_code)
+        result += elias_encoder.encode_single_value(no_of_unique)
 
-    return result
+        for char in unique_chars_list:
+            ascii_8_bit = self.convert_to_8_bit_ascii(char)
+            huffman_code = huffman_values[char]
+            encoded_huffman_len = elias_encoder.encode_single_value(
+                len(huffman_code)
+            )
 
+            result += ascii_8_bit
+            result += encoded_huffman_len
+            result += huffman_code
+            print(ascii_8_bit)
+            print(encoded_huffman_len)
+            print(huffman_code)
 
-def convert_to_8_bit_ascii(char: str) -> str:
-    return "{0:0=8d}".format(int(bin(ord(char))[2:]))
+        return result
+
+    @staticmethod
+    def convert_to_8_bit_ascii(char: str) -> str:
+        return "{0:0=8d}".format(int(bin(ord(char))[2:]))
--- assignments/assignment03/q1/lzss_encoder_test.py
+++ assignments/assignment03/q1/lzss_encoder_test.py
@@ -4,5 +4,6 @@ from . import lzss_encoder
 class TestLZSSEncoder:
     def test_from_assignment_sheet(self):
         expected = "011011000011101100010010000110001101001"
-        result = lzss_encoder.encode_header("aacaacabcaba", 6, 4)
+        encoder = lzss_encoder.Encoder("aacaacabcaba", 6, 40)
+        result = encoder.encode_header()
         assert expected == result
